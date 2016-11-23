dd2cf75a54992b6759622dea8411ab7d40b746d8
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2016-11-23 17:34:56 +0000

    Added more Ruby files

---
+++ Week1/Ruby/Lambdas.rb
@@ -0,0 +1,5 @@
+# Create new Lambda
+my_lambda = lambda {|name| puts "Hello, #{name}!"}
+
+# Execute Lambda
+my_lambda.call('Adi')
---
+++ Week1/Ruby/Methods.rb
@@ -0,0 +1,7 @@
+# Create Methon
+def hello(name)
+  puts "Welcome to learning how to code #{name}"
+end
+
+# Execute Method
+hello("Adi")
