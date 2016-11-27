0ddfcdb3746e1fab199d6bde12c5980491a34756
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2016-11-27 14:42:13 +0000

    Removed Chris Pines files becuase they were in the wrong place

--- Week2/chap02/ex1.rb
+++
@@ -1,9 +0,0 @@
-puts 'Hello, world!'
-puts ''
-puts 'Good-bye.'
-
-puts "I like " + "apple pie."
-
-puts 'blink ' * 4
-
-puts 'You\'re swell"'
--- Week2/ex1.rb
+++
@@ -1,5 +0,0 @@
-var = "just another " + "string"
-puts var
-
-var = 5 * (1+2)
-puts var
