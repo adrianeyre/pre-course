8bc0495665151e4cc61ea7b1fdd544f51a485b76
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2016-11-29 16:38:23 +0000

    Added Ruby ex1, ex2 and ex3

---
+++ Week2/ruby-kickstart/chap01/ex1.rb
@@ -0,0 +1,19 @@
+# fill out the method below
+# then test tho see if you did them correctly with
+#  $ rake 1:1
+
+# Given a number, return 20 less than, that number multiplied by 5
+#
+# arithmetic1(10)     # => 30
+# arithmeitc1(10.5)   # => 32.5
+# arithmeitc1(-6)     # => -50
+
+# My Solution
+def arithmetic1(n)
+  n * 5 - 20
+end
+
+# Suggested Solution
+def arithmetic1(n)
+  n * 5 - 20
+end
---
+++ Week2/ruby-kickstart/chap01/ex3.rb
@@ -0,0 +1,22 @@
+# remember, you can test this file with
+#   $ rake 1:3
+
+
+# Given a number, n, return 10 if it is even, and 20 if it is odd
+#
+# ten_twenty(5) # => 20
+# ten_twenty(6) # => 10
+
+# My Solution
+def ten_twenty(n)
+  n.even? ? 10 : 20
+end
+
+# Suggested Solution
+def ten_twenty(n)
+  if n % 2 == 0
+    10
+  else
+    20
+  end
+end
